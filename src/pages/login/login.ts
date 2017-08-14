80111d9eff1964f788a06ddb64085b6d606661f8
robbin <robbin.luo@huawei.com>
2017-08-14 21:51:05 +0800

    a

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -19,8 +19,8 @@ export class LoginPage {
   debug = false;
   constructor(public modalCtrl: ModalController, public fb: FormBuilder, public navCtrl: NavController,public navParams: NavParams) {
     this.LoginForm = fb.group({
-      username: ['', Validators.pattern(/^([\w\d]{6,16})|(1[0-9]{10})|(([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+)$/)],
-      password: ['', Validators.pattern(/^[\w\d]{6,16}$/)]
+      username: ['', Validators.pattern(/(^[a-zA-Z][\w\d]{2,19}$)|(^1[0-9]{10}$)|(^[a-zA-Z0-9][a-zA-Z0-9_\.\-]*\@[a-zA-Z0-9][a-zA-Z0-9\-]*(\.[a-zA-Z0-9]{2,4}){1,3}$)/)],
+      password: ['', Validators.pattern(/^[a-zA-Z0-9`\-=\[\];,./~!@#$%^*()_+}{:?]{6,16}$/)]
     });
   }
 
