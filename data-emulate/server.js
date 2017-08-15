cf023c5bdf98b5e204b9ab99ba7ca1275795aaa6
richardgong1987 <909253305@qq.com>
2017-08-15 19:14:33 +0800

    a

--- data-emulate/server.js
+++ data-emulate/server.js
@@ -28,6 +28,42 @@ app.post('/mobile-h5-auth/login', ({body}, res) => {
 });
 
 
+/**
+ * 1。*****************首页数据，开始******************
+ */
+let _token = 'uk9ZswIRWYIdKWmx93k3apeplMuDBH4Y2KuvkLlV';
+app.get('/mobileh5-announcements/banner', ({body}, res) => {
+  res.json(require('./data/index/get/banner.json'));
+});
+
+
+app.get('/mobileh5-users/user-account-info', ({body}, res) => {
+  res.json(require('./data/index/get/user-account-info.json'));
+});
+
+app.get('/mobileh5-announcements', ({body}, res) => {
+  res.json(require('./data/index/get/mobileh5-announcements.json'));
+});
+
+let errortoken = {
+  Msg: "token",
+  isSuccess: 0,
+  type: 'token'
+};
+
+app.post('/mobile-lotteries-h5/lottery-info', ({body}, res) => {
+  if (body._token == _token) {
+    res.json(require('./data/index/post/lottery-info.json'));
+  } else {
+    res.json(errortoken);
+  }
+});
+
+app.get('/mobileh5-users/user-account-info', ({body}, res) => {
+  res.json(require('./data/index/get/user-account-info.json'));
+});
+
+
 app.listen(8181);
 
 console.log('************listening************* port 8181');
