6b7c9658b306de36b2c07a94476270ccdd9de0fa
robbin <robbin.luo@huawei.com>
2017-08-23 00:40:41 +0800

    a

--- src/pages/personal/personal-profiles/personal-profiles.module.ts
+++ src/pages/personal/personal-profiles/personal-profiles.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { PersonalProfilesPage } from './personal-profiles';
 import {ComponentsModule} from "../../../components/components.module";
+import {BalanceProvider} from "../../../providers/service/balance/balance";
+import {BankcardService} from "../../../providers/service/fund-service/bankcard-service";
 
 @NgModule({
   declarations: [
@@ -11,6 +13,6 @@ import {ComponentsModule} from "../../../components/components.module";
     ComponentsModule,
     IonicPageModule.forChild(PersonalProfilesPage),
   ],
-  providers:[]
+  providers:[BalanceProvider,BankcardService]
 })
 export class PersonalProfilesPageModule {}
--- src/pages/personal/personal-profiles/personal-profiles.ts
+++ src/pages/personal/personal-profiles/personal-profiles.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {IonicPage, ModalController, NavController, NavParams} from 'ionic-angular';
 import {GlobalShareProvider} from "../../../providers/global-share/global-share";
 import {Config} from "../../../config/config";
-
+import {BalanceProvider} from "../../../providers/service/balance/balance";
+import {BankcardService} from "../../../providers/service/fund-service/bankcard-service";
 
 @IonicPage()
 @Component({
@@ -11,37 +12,39 @@ import {Config} from "../../../config/config";
 })
 export class PersonalProfilesPage {
   userTypeMap = Config.userTypeMap;
-  constructor(public navCtrl: NavController, public navParams: NavParams,public share:GlobalShareProvider) {
+  moneyDisplay=false;
+  star= '******';
+
+  constructor(public bankcard:BankcardService,public modalCtrl: ModalController,public balance:BalanceProvider,public navCtrl: NavController, public navParams: NavParams,public share:GlobalShareProvider) {
+    this.balance.getUserBalance();
+    this.bankcard.getRemoteServer();
   }
-
-  ionViewDidLoad() {
-
-  }
-
-
 
   lineService() {
-    // function PlatformDetected() {
-    //   var userAgent: any = navigator.userAgent.toLowerCase();
-    //   this.isIpad = userAgent.match(/ipad/i) == "ipad";
-    //   this.isIphoneOs = userAgent.match(/iphone os/i) == "iphone os";
-    //   this.isMidp = userAgent.match(/midp/i) == "midp";
-    //   this.isUc7 = userAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
-    //   this.isUc = userAgent.match(/ucweb/i) == "ucweb";
-    //   this.isAndroid = userAgent.match(/android/i) == "android";
-    //   this.isCE = userAgent.match(/windows ce/i) == "windows ce";
-    //   this.isWM = userAgent.match(/windows mobile/i) == "windows mobile";
-    //   this.isPhone = this.isIpad || this.isIphoneOs || this.isMidp || this.isUc7 || this.isUc || this.isAndroid || this.isCE || this.isWM;
-    // }
-    // var platformInstance = new PlatformDetected();
-    // if (platformInstance.isIphoneOs) {
-    //   window.open(`https://vp9.live800.com/live800/chatClient/chatbox.jsp?companyID=80000041&configID=2083&codeType=custom&info=loginname=${this.usr.tmpUserInfo.data.username}&name=${this.usr.tmpUserInfo.data.username}&hashCode=&amp;timestamp=${Date.now()}`,'_blank')
-    //   return;
-    // }
-    // let profileModal = this.modalCtrl.create(Onlineservice, {data: {username:this.usr.tmpUserInfo.data.username}, title: ''});
-    // profileModal.present();
+    function PlatformDetected() {
+      var userAgent: any = navigator.userAgent.toLowerCase();
+      this.isIpad = userAgent.match(/ipad/i) == "ipad";
+      this.isIphoneOs = userAgent.match(/iphone os/i) == "iphone os";
+      this.isMidp = userAgent.match(/midp/i) == "midp";
+      this.isUc7 = userAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+      this.isUc = userAgent.match(/ucweb/i) == "ucweb";
+      this.isAndroid = userAgent.match(/android/i) == "android";
+      this.isCE = userAgent.match(/windows ce/i) == "windows ce";
+      this.isWM = userAgent.match(/windows mobile/i) == "windows mobile";
+      this.isPhone = this.isIpad || this.isIphoneOs || this.isMidp || this.isUc7 || this.isUc || this.isAndroid || this.isCE || this.isWM;
+    }
+    var platformInstance = new PlatformDetected();
+    if (platformInstance.isIphoneOs) {
+      window.open(`https://vp9.live800.com/live800/chatClient/chatbox.jsp?companyID=80000041&configID=2083&codeType=custom&info=loginname=${this.share.user.username}&name=${this.share.user.username}&hashCode=&amp;timestamp=${Date.now()}`,'_blank')
+      return;
+    }
+    let profileModal = this.modalCtrl.create('Onlineservice', {data: {username:this.share.user.username}, title: ''});
+    profileModal.present();
   }
 
+  hideMoney(){
+    this.moneyDisplay = !this.moneyDisplay;
+  }
 
   pushPage(page,item?){
     if(page) this.navCtrl.push(page,item);
