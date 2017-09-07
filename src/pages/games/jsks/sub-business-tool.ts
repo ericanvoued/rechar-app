1d8db3af6f0d736cb9773aff4ef38587269ebc92
richardgong1987 <909253305@qq.com>
2017-09-07 20:46:05 +0800

    a

---
+++ src/pages/games/jsks/sub-business-tool.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import {BusinessTool} from "../../../providers/tools/business-tool";
+
+@Injectable()
+export class SubBusinessToolProvider extends BusinessTool{
+
+}
