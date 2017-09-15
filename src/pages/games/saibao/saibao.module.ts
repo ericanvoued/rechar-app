b06bf6c5b9062b2f285486ff9c8a7f55a8d76048
richardgong1987 <909253305@qq.com>
2017-09-15 13:38:25 +0800

    a

--- src/pages/games/saibao/saibao.module.ts
+++ src/pages/games/saibao/saibao.module.ts
@@ -1,6 +1,13 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SaibaoPage } from './saibao';
+import {SubBusinessToolProvider} from "./sub-business-tool";
+import {SubCameconfigServiceProvider} from "./subCameconfigServiceProvider";
+import {GameconfigServiceProvider} from "../../../providers/service/games/gameconfig-service/gameconfig-service";
+import {BasketServiceProvider} from "../../../providers/service/games/basket-service/basket-service";
+import {UserbalanceServiceProvider} from "../../../providers/service/userbalance-service/userbalance-service";
+import {Gamelist} from "../../../providers/service/games/gamelist-service";
+import {BusinessTool} from "../../../providers/tools/business-tool";
 
 @NgModule({
   declarations: [
@@ -9,5 +16,14 @@ import { SaibaoPage } from './saibao';
   imports: [
     IonicPageModule.forChild(SaibaoPage),
   ],
+  providers: [
+    SubBusinessToolProvider,
+    SubCameconfigServiceProvider,
+    GameconfigServiceProvider,
+    BasketServiceProvider,
+    UserbalanceServiceProvider,
+    Gamelist,
+    BusinessTool
+  ]
 })
 export class SaibaoPageModule {}
--- src/pages/games/saibao/saibao.ts
+++ src/pages/games/saibao/saibao.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, MenuController, NavController, NavParams, ToastController} from 'ionic-angular';
+import {Config} from "../../../config/config";
+import {GlobalShareProvider} from "../../../providers/global-share/global-share";
+import {SubBusinessToolProvider} from "./sub-business-tool";
+import {Gamelist} from "../../../providers/service/games/gamelist-service";
+import {BasketServiceProvider} from "../../../providers/service/games/basket-service/basket-service";
+import {SubCameconfigServiceProvider} from "./subCameconfigServiceProvider";
 
 /**
  * Generated class for the SaibaoPage page.
@@ -15,13 +21,226 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SaibaoPage {
 
+  c: any = {selectarea: [], selectareaPair: []};
+  b: any = {selectarea: [], selectareaPair: []};
+  a: any = {selectarea: [], selectareaPair: []};
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  methodGroup: {
+    a: any;
+    b: any;
+    c: any;
+  };
+  ballLabelMap = Config.ballLabelMap;
+  private cccInterval: number;
+  ccc: boolean;
+  yearReg = /[\d]{4}-/;
 
+  constructor(public share: GlobalShareProvider, public util: SubBusinessToolProvider, private  gameinfo: Gamelist, public basket: BasketServiceProvider, private gameconfigdata: SubCameconfigServiceProvider, public menuCtrl: MenuController, public navCtrl: NavController, public  navParams: NavParams, public toastCtrl: ToastController) {
+    this.other();
+    let nav = this.navParams.get('nav') || {};
+    let gamenav = nav;
+    this.gameconfigdata.setPid(gamenav.pid);
+    this.gameconfigdata.fetchMethedsList();
+    this.share.gameId = nav && nav.pid;
+    gameconfigdata.getDefaultsMethods();
+    gameconfigdata.isInit = true;
+    gameconfigdata.getIssues();
+    this.gameinfo.getRecord();
+    basket.clearAll();
+    this.menuCtrl.enable(false, 'unauthenticated');
+  }
+
+  selectall(arr: Array<any>) {
+    if (arr.every(v => v)) {
+      arr.forEach((v, k, arr) => arr[k] = false);
+    } else {
+      arr.forEach((v, k, arr) => arr[k] = true);
+    }
+
+  }
+
+  tmpComformMethod(a, b, c) {
+    this.methodGroup = {
+      a, b, c
+    }
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad SaibaoPage');
+
   }
+
+  ionViewWillLeave() {
+    this.menuCtrl.enable(true, 'unauthenticated');
+  }
+
+  private other() {
+    this.menuCtrl.enable(false, 'unauthenticated');
+    clearInterval(this.cccInterval);
+    this.cccInterval = setInterval(() => {
+      this.ccc = !this.ccc;
+    }, 800);
+
+  }
+
+  setGrounpChoose(name, arr, value) {
+    this.util.daxiaodanshuangqing(arr, name, value);
+  }
+
+  comformMethod() {
+    if (this.methodGroup && this.methodGroup.a && this.methodGroup.b && this.methodGroup.c) {
+      this.a = this.gameconfigdata.defaultsMethodData.a = this.methodGroup.a;
+      this.b = this.gameconfigdata.defaultsMethodData.b = this.methodGroup.b;
+      this.c = this.gameconfigdata.defaultsMethodData.c = this.methodGroup.c;
+    }
+
+  }
+
+  goHelpPage() {
+    this.navCtrl.push("PlayHelpPage");
+  }
+
+  mindus(obj) {
+    obj.mutipleAndModeObj.times--;
+    obj.mutipleAndModeObj.times = Math.max(obj.mutipleAndModeObj.times, 1);
+
+  }
+
+  pluse(obj) {
+    obj.mutipleAndModeObj.times++;
+    obj.mutipleAndModeObj.times = Math.min(obj.mutipleAndModeObj.times, obj.max_multiple);
+
+  }
+
+  pluseOrmindusOnInput = debounce((obj, e) => {
+    this.pluseOrmindus(obj, e);
+  }, 1000)
+
+  pluseOrmindus(obj, e) {
+    let v = +e.target.value;
+    if (v <= 1) {
+      obj.mutipleAndModeObj.times = 1;
+    } else if (v >= obj.max_multiple) {
+      obj.mutipleAndModeObj.times = obj.max_multiple;
+    } else {
+      obj.mutipleAndModeObj.times = v;
+    }
+    e.target.value = obj.mutipleAndModeObj.times;
+  }
+
+  modeChange(obj, mode) {
+    if (!obj.oldmax_multiple) {
+      obj.oldmax_multiple = obj.max_multiple;
+    }
+
+    obj.mutipleAndModeObj.mode = mode;
+    obj.max_multiple = obj.oldmax_multiple / mode;
+    this.restTimesWhenChangeMode(obj);
+  }
+
+  restTimesWhenChangeMode(obj) {
+    if (obj.mutipleAndModeObj.times >= obj.max_multiple) {
+      obj.mutipleAndModeObj.times = obj.max_multiple;
+    }
+  }
+
+  messages(obj): void {
+    let hasChoose = this.basket.hasChooseBall(obj.selectarea);
+    if (hasChoose && (obj.count == 0)) {
+      let toast = this.toastCtrl.create({
+        message: obj.bet_note,
+        duration: 1000,
+        position: 'middle'
+      });
+      toast.present(toast);
+    } else if (obj.isRedudu) {
+      let toast = this.toastCtrl.create({
+        message: '订单已经存在',
+        duration: 1000,
+        position: 'middle'
+      });
+
+      toast.present(toast);
+    } else {
+      let toast = this.toastCtrl.create({
+        message: '请选择注单',
+        duration: 1000,
+        position: 'middle'
+      });
+      toast.present(toast);
+    }
+  }
+
+  addBall(obj): boolean {
+    let isSucess = this.basket.addDataToBasketK3(obj);
+
+    if (isSucess) {
+      this.clear(obj);
+      let toast = this.toastCtrl.create({
+        message: '注单添加成功',
+        duration: 1000,
+        position: 'bottom'
+      });
+      toast.present(toast);
+    } else {
+      this.messages(obj);
+    }
+
+    return isSucess;
+  }
+
+  goBuybasket(obj) {
+    let isSucess = this.basket.addDataToBasketK3(obj);
+
+    if (isSucess || this.share.basketData.length) {
+      this.clear(obj);
+      this.navCtrl.push("BuyBasketPage", {nav: this.navParams.get('nav')});
+    } else {
+      this.messages(obj);
+    }
+
+  }
+
+  randomBall(obj) {
+    this.util.findRandom(obj.name_cn, obj);
+  }
+
+  clear(obj) {
+    obj.mutipleAndModeObj.times = 1;
+    this.util.clearBall(obj.selectarea);
+  }
+
+}
+
+
+function debounce(func, wait?, immediate?) {
+  var timeout, args, context, timestamp, result;
+
+  var later = function () {
+    var last = new Date().getTime() - timestamp;
+
+    if (last < wait && last >= 0) {
+      timeout = setTimeout(later, wait - last);
+    } else {
+      timeout = null;
+      if (!immediate) {
+        result = func.apply(context, args);
+        if (!timeout) context = args = null;
+      }
+    }
+  };
+
+  return function () {
+    context = this;
+    args = arguments;
+    timestamp = new Date().getTime();
+    var callNow = immediate && !timeout;
+    if (!timeout) timeout = setTimeout(later, wait);
+    if (callNow) {
+      result = func.apply(context, args);
+      context = args = null;
+    }
+
+    return result;
+  };
 
 }
